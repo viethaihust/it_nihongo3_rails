3a4e195e0a5fee279443f60fdb9b0dd8a642556c
viethaihust <vuviethaihust@gmail.com>
2023-02-02 08:55:17 +0000

    modify name to review list

--- app/controllers/book_reviews_controller.rb
+++ app/controllers/book_reviews_controller.rb
@@ -22,7 +22,8 @@ class BookReviewsController < ApplicationController
   # POST /book_reviews or /book_reviews.json
   def create
     @book_review = BookReview.new(book_review_params)
-
+    @book_review.user_id = current_user.id
+    
     respond_to do |format|
       if @book_review.save
         url = "/books/" + @book_review.book_id.to_s
